121c631d082cf6afcec2a7d860a7ff5efe73910b
Have Samuel <samiehave9@gmail.com>
2022-06-16 18:51:25 +0300

    Making changes

--- person.rb
+++ person.rb
@@ -4,20 +4,18 @@ class Person < Nameable
   attr_reader :id, :rentals
   attr_accessor :name, :age
 
-  def initialize(age, name = 'unknown', parent_permission = true)
+  def initialize(age, name = 'unknown', parent_permission = true) # rubocop:disable Style/OptionalBooleanParameter
     super()
-    @id = id
+    @parent_permission = parent_permission
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @rentals = []
-    @parent_permission = parent_permission
   end
 
   # Adding Getters and setters / you can make it even shorter by using: attr_accessor :name, ad, age
-  def can_use_services
-    return true if of_age? || @parent_permission
-
-    false
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
   def correct_name
