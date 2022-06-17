5fa8ee01de461b422c34d622d25c7777cafd4121
Have Samuel <samiehave9@gmail.com>
2022-06-17 15:22:29 +0300

    Some changes in the student file

--- student.rb
+++ student.rb
@@ -1,12 +1,20 @@
 require './person'
+require './classroom'
 
 class Student < Person
-  def initialize(age, classroom, name = 'unknown', parent_permission: true)
-    super(name, age, parent_permission)
+  att_reader :classroom
+
+  def initialize(age, classroom, name = 'unknown', parent_permission)
+    super(name, age, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
     '¯\(ツ)/¯'
+  end
+
+  def classroom = (classroom)
+    @classroom = classroom
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 end
