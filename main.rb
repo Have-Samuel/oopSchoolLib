29c1d7445d378455d1c58c0e34139b607efd72d8
Have Samuel <samiehave9@gmail.com>
2022-06-17 21:51:31 +0300

    Checking for Linter\ errors

--- main.rb
+++ main.rb
@@ -154,10 +154,10 @@ class Main # rubocop:disable Metrics/ClassLength
     book_index = gets.chomp.to_i
     puts 'select a person from the following list by number (not ID):'
     @list_people.each_with_index do |person, index|
-    puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
+      puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
     end
     person_index = gets.chomp.to_i
-    puts 'Enter the Date of rental (YYY-MM-DD): '
+    puts 'Enter the Date of rental (YYYY-MM-DD): '
     date = gets.chomp
     rental = Rental.new(date, @book_list[book_index], @list_people[person_index])
     @rentals.push(rental)
@@ -165,16 +165,6 @@ class Main # rubocop:disable Metrics/ClassLength
     puts 'Rental created'
     prompt_user
   end
-
-  # def add_rental
-  #    puts 'select a book from the following list by number (not ID):'
-  #     @booklist.each_with_index do |book, index| puts "[#{index}] Title: #{book.title}, Author: #{book.author}"
-  #      end book_index = gets.chomp.to_i
-  #       puts 'select a person from the following list by number (not ID):'
-  #        @list_people.each_with_index do |person, index|
-  #          puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
-  #          end person_index = gets.chomp.to_i
-  #           puts 'Enter the Date of rental (YYYY-MM-DD):' date = gets.chomp rental = Rental.new(date, @booklist[book_index], @list_people[person_index]) @rentals.push(rental) puts 'Rental created!' prompt_user end 
 
   def list_all_rental_by_id
     print 'ID of person: '
