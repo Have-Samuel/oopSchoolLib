7d351d88aeddb5894a4c8962e700b03bac6aa13e
Have Samuel <samiehave9@gmail.com>
2022-06-17 21:46:02 +0300

    Checking for Linter\ errors

--- main.rb
+++ main.rb
@@ -6,7 +6,7 @@ require './rental'
 
 class Main # rubocop:disable Metrics/ClassLength
   def initialize
-    @book_ist = []
+    @book_list = []
     @list_people = []
     @rentals = []
   end
@@ -43,7 +43,7 @@ class Main # rubocop:disable Metrics/ClassLength
     puts '0 - Exit'
   end
 
-  def selection
+  def selection(input)
     case input
     when '1'
       add_user
@@ -67,12 +67,12 @@ class Main # rubocop:disable Metrics/ClassLength
     print 'Author: '
     author = gets.chomp
     book = Book.new(title, author)
-    @booklist.push(book)
+    @book_list.push(book)
     response('Book')
   end
 
   def list_all_books
-    @booklist.each do |book|
+    @book_list.each do |book|
       puts "Title: #{book.title} | Author: #{book.author}"
     end
     puts "\n"
@@ -116,9 +116,9 @@ class Main # rubocop:disable Metrics/ClassLength
 
     parent_permission = permission.downcase == 'y' || permission.downcase == 'yes' || permission == ''
 
-    Student.new(age, name, classroom, parent_permission: parent_permission)
+    student = Student.new(age, name, classroom, parent_permission: parent_permission)
 
-    @list_people.push('student')
+    @list_people.push(student)
 
     response('Student')
   end
@@ -148,25 +148,40 @@ class Main # rubocop:disable Metrics/ClassLength
 
   def add_rental
     puts 'Select a book from the following list by number (not ID): '
-    @booklist.each_with_index do |_book, index|
-      puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
+    @book_list.each_with_index do |book, index|
+    puts "[#{index}] Title: #{book.title}, Author: #{book.author}"
+    end
+    book_index = gets.chomp.to_i
+    puts 'select a person from the following list by number (not ID):'
+    @list_people.each_with_index do |person, index|
+    puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
     end
     person_index = gets.chomp.to_i
     puts 'Enter the Date of rental (YYY-MM-DD): '
     date = gets.chomp
-    rental = Rental.new(date, @booklist[book_index], @list_people[person_index])
+    rental = Rental.new(date, @book_list[book_index], @list_people[person_index])
     @rentals.push(rental)
 
     puts 'Rental created'
     prompt_user
   end
 
+  # def add_rental
+  #    puts 'select a book from the following list by number (not ID):'
+  #     @booklist.each_with_index do |book, index| puts "[#{index}] Title: #{book.title}, Author: #{book.author}"
+  #      end book_index = gets.chomp.to_i
+  #       puts 'select a person from the following list by number (not ID):'
+  #        @list_people.each_with_index do |person, index|
+  #          puts "[#{index}] [#{person.class}] Name: #{person.name} ID: #{person.id} Age: #{person.age}"
+  #          end person_index = gets.chomp.to_i
+  #           puts 'Enter the Date of rental (YYYY-MM-DD):' date = gets.chomp rental = Rental.new(date, @booklist[book_index], @list_people[person_index]) @rentals.push(rental) puts 'Rental created!' prompt_user end 
+
   def list_all_rental_by_id
     print 'ID of person: '
     user_id = gets.chomp.to_i
 
     puts "Rentals: #{user_id}"
-    @rental.each do |rental|
+    @rentals.each do |rental|
       if rental.person.id == user_id
         puts "[#{rental.person.class}] Name: #{rental.person.name}
         | Date: #{rental.date} | Book: \"#{rental.book.title}\" by #{rental.book.author}"
